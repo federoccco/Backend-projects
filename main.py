73bfa7fbaeaf90b7f48c1c1cb36a7e7ff47d9ab9
federoccco <greco.federico92@gmail.com>
2022-11-18 16:59:38 +0100

    homepage e sum page

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+# import la classe FastAPI dal modulo fastpi
 from fastapi import FastAPI
 
+# Creazione dell'istanza
 app = FastAPI()
 
+# Homepage
 @app.get('/')
 def root():
     return '''Benvenuto nella calcolatrice più inutile e inefficiente del web, per sapere la somma di due numeri digita nella barra dell'URL: <Home page URL>/num1/num2'''
 
+# Somma di due numeri 
 @app.get('/{num1}/{num2}')
 def sum(num1 : int, num2 : int):
     '''restituisce a schermo la somma di due numeri'''
